59c54086229325bba1d20d93459d0673958e9744
jvmntr <jvmntr.email.com@gmail.com>
2025-08-12 17:14:40 -0300

    [UPT] Implementado a lógica de requisição ao carregar a página

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,29 @@
 const api_key = "7ddc0ca5-b2e5-4c38-a723-ae4dbc85e762"
 
+const randomGenerator = Math.floor(Math.random() * 100) + 1;
+
+const api_url = `https://api.pokemontcg.io/v2/cards?page=${randomGenerator}&pageSize=250&select=images`
+
 let cartas = [];
 
 document.addEventListener("DOMContentLoaded", () => {
-    const api_url_2 = `https://api.pokemontcg.io/v2/cards?page=7&pageSize=250&select=images`
-
-    fetch(api_url_2, {
+    console.log("Iniciando...")
+    fetch(api_url, {
         headers: { "X-Api-Key": api_key }
     })
         .then(res => res.json())
         .then(data => {
-            data.results.forEach(data => {
-                cartas.push(cartas);
+            data.data.forEach(card => {
+                cartas.push(card);
             })
+            console.log(cartas)
         })
+        .catch(err => console.error("Erro na requisição.", err))
 })
 
-console.log(cartas)
+
 
 document.getElementById("containerButton").addEventListener("click", () => {
-    const randomGenerator = Math.floor(Math.random() * 100) + 1;
-    const api_url = `https://api.pokemontcg.io/v2/cards?page=${randomGenerator}&pageSize=5&select=images`
 
     fetch(api_url, {
         headers: { "X-Api-Key": api_key }
@@ -37,5 +40,5 @@ document.getElementById("containerButton").addEventListener("click", () => {
                 container.appendChild(img);
             });
         })
-        .catch(err => console.log("Erro na busca.", err));
+        .catch(err => console.error("Erro na busca.", err));
 })
